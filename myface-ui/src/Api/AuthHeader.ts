5d0f0d787f41e5764fc85dc271016acf0b938e2d
Jordan <jordan.d.carlton@gmail.com>
2021-11-09 10:07:52 +0000

    Add auth header encode

---
+++ myface-ui/src/Api/AuthHeader.ts
@@ -0,0 +1,6 @@
+const encodeHeader = (username: string, password: string) => {
+    const toEncode = username + ':' + password;
+    return "Authorization: Basic "+ btoa(toEncode);
+}
+
+export default encodeHeader
