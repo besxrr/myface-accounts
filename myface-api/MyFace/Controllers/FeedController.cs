31f1e7918e52f89908b73c4ee8ea3f067aabfee4
Jordan <jordan.d.carlton@gmail.com>
2021-11-10 12:37:47 +0000

    Add authentication for Get Feed in FeedController.cs

--- myface-api/MyFace/Controllers/FeedController.cs
+++ myface-api/MyFace/Controllers/FeedController.cs
@@ -2,22 +2,30 @@
 using MyFace.Models.Request;
 using MyFace.Models.Response;
 using MyFace.Repositories;
+using static MyFace.Helpers.AuthorizationHelper;
 
 namespace MyFace.Controllers
 {
     [Route("feed")]
-    public class FeedController
+    public class FeedController: ControllerBase
     {
         private readonly IPostsRepo _posts;
+        private readonly IUsersRepo _users;
 
-        public FeedController(IPostsRepo posts)
+        public FeedController(IPostsRepo posts, IUsersRepo users)
         {
             _posts = posts;
+            _users = users;
         }
         
         [HttpGet("")]
         public ActionResult<FeedModel> GetFeed([FromQuery] FeedSearchRequest searchRequest)
         {
+            if (!IsUserAuthorized(_users, Request))
+            {
+                return StatusCode(401, "Authentication Failed!");
+            }
+
             var posts = _posts.SearchFeed(searchRequest);
             var postCount = _posts.Count(searchRequest);
             return FeedModel.Create(searchRequest, posts, postCount);
